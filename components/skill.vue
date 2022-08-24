2ca90df6c0ecc2c6e875af24f7961b34e58b2248
chisomgbata <chisomgbata@gmail.com>
2022-08-24 03:31:51 +0100

    added skills section mobile

---
+++ components/skill.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="flex gap-2 items-center">
+      <span class="bg-primary p-2 rounded-full "
+        ><img :src="`/icons/${skill.type}.svg`" :alt="skill.type"
+      /></span>
+      <h2 class="text-base font-semibold">{{ skill.type }}</h2>
+    </div>
+    <div class="mt-10 flex flex-col gap-10 text-sm">
+      <div class="flex flex-col gap-2" v-for="item in skill.skill">
+        <div class="flex justify-between items-baseline">
+          <span class=""
+            ><img :src="`/icons/${item.iconLink}.svg`" :alt="item.name"
+          /></span>
+          <p>{{ item.name }}</p>
+        </div>
+        <div class="w-full rounded-xl h-1 bg-primary"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  interface skill {
+    type: string;
+    skill: {
+      iconLink: string;
+      name: string;
+    }[];
+  }
+  defineProps<{
+    skill: skill;
+  }>();
+</script>
---
+++ components/skills.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="rounded-lg bg-white py-16 mt-6 px-12">
+    <section>
+      <h1 class="font-bold text-2xl text-center">My Skills</h1>
+      <div class="flex flex-col gap-20 mt-16">
+        <div v-for="skill in skills">
+          <skill :skill="skill" />
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const skills = [
+    {
+      type: "Language",
+      skill: [
+        {
+          iconLink: "js",
+          name: "Javascript",
+        },
+        {
+          iconLink: "php",
+          name: "PHP",
+        },
+      ],
+    },
+    {
+      type: "Tools",
+      skill: [
+        {
+          iconLink: "linux",
+          name: "Linux",
+        },
+        {
+          iconLink: "git",
+          name: "Git",
+        },
+      ],
+    },
+    {
+      type: "Frameworks",
+      skill: [
+        {
+          iconLink: "vue",
+          name: "Vue",
+        },
+        {
+          iconLink: "laravel",
+          name: "Laravel",
+        },
+      ],
+    },
+  ];
+</script>
