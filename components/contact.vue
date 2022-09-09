c1e834dbde7fe00f90d6872bf4632f440edcd2f6
chisom <chisomgbata@gmail.com>
2022-09-09 10:33:37 +0100

    netlify-form v5

--- components/contact.vue
+++ components/contact.vue
@@ -6,7 +6,7 @@
       <img src="/icons/phone.svg" alt="Phone" />
       <img src="/icons/twitter.svg" alt="Twitter" />
     </div>
-    <form  class="mt-10" data-netlify="true">
+    <form  class="mt-10" data-netlify="true" method="post">
       <h2 class="font-bold text-2xl">Send Me A Message</h2>
       <div class="bg-white rounded-lg px-3 py-6 flex flex-col gap-4 mt-14">
         <Cinput name="name" label="Name" type="text" />
