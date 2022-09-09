f627a9fe9e860c70ba02af9ccceb008fad69aa79
chisom <chisomgbata@gmail.com>
2022-09-09 10:48:35 +0100

    netlify-form test v2

--- components/contact.vue
+++ components/contact.vue
@@ -21,7 +21,7 @@
       </div>
     </form>
   </div> -->
-  <form name="contact" method="POST" data-netlify="true">
+  <form name="contact" method="POST" netlify>
     <p>
       <label>Your Name: <input type="text" name="name" /></label>
     </p>
